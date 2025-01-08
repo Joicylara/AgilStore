271ec9f4b4f32894a9691f55aefc44205fcd7c6e
joicy kelly <joicykelly2301@gmail.com>
2025-01-08 18:19:39 -0300

    feat: adicionando cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import { rota } from "./routes/agilStore.routes.js";
 
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(rota);
 
